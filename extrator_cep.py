1a0849918f9f5ef0972494e967fd5957a0d90c84
Gabi Desktop <gabii12303.net@gmail.com>
2023-04-24 15:22:59 -0300

    part 9 - Quantificadores e Intervalos

--- extrator_cep.py
+++ extrator_cep.py
@@ -1,10 +1,10 @@
 import re  # Regular Expression -- RegEx
-endereco = "Rua das Flores 72, apartamento 1002, Laranjeiras, Rio de Janeiro, RJ, 23440S120"
+endereco = "Rua da Flores 72, apartamento 1002, Laranjeiras, Rio de Janeiro, RJ, 23440-120"
 
-padrao = re.compile(
-    "[0123456789][0123456789][0123456789][0123456789][0123456789][-]?[0123456789][0123456789][0123456789]")
-busca = padrao.search(endereco)  # return Match
+# 5 dígitos + hífen (opcional) + 3 dígitos
 
+padrao = re.compile("[0-9]{5}[-]{0,1}[0-9]{3}")
+busca = padrao.search(endereco)  # Match
 if busca:
     cep = busca.group()
     print(cep)
